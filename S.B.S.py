9b5ddadea8faea59ab2b7dcd3e1d7424cf4b9bf4
Cartoonstone <160048487+Cartoonstone@users.noreply.github.com>
2024-12-05 14:30:28 +0000

    Update S.B.S.py

--- S.B.S.py
+++ S.B.S.py
@@ -1,22 +1,47 @@
+from machine import Pin, PWM, ADC
+from time import sleep_ms
+ljospinnar = [11,12,13,14,15,16]
+def ljosoff():
+    for i in ljospinnar:
+        i.value(0)
+    sleep_ms(2000)
+        
 stig = []
 
-user1_sk = 1
-user1_bl = 1
-user1_st = 1
+user1_sk = Pin(12, Pin.IN, Pin.PULL_UP)
+user1_bl = Pin(12, Pin.IN, Pin.PULL_UP)
+user1_st = Pin(12, Pin.IN, Pin.PULL_UP)
 
-user2_sk = 1
-user2_bl = 1
-user2_st = 1
+user2_sk = Pin(12, Pin.IN, Pin.PULL_UP)
+user2_bl = Pin(12, Pin.IN, Pin.PULL_UP)
+user2_st = Pin(12, Pin.IN, Pin.PULL_UP)
+
+user1_rautt = Pin(9, Pin.OUT)
+user1_graent = Pin(9, Pin.OUT)
+user1_blatt = Pin(9, Pin.OUT)
+
+user2_rautt = Pin(9, Pin.OUT)
+user2_graent = Pin(9, Pin.OUT)
+user2_blatt = Pin(9, Pin.OUT)
+
 
 
 while True:
     
     while True:
+        user1_rautt.value(1)
+        user1_graent.value(1)
+        user1_blatt.value(1)
+
+        user2_rautt.value(1)
+        user2_graent.value(1)
+        user2_blatt.value(1)
+        
         user1_input_sk = False
-        user1_input_bl = True
+        user1_input_bl = False
         user1_input_st = False
 
-        user2_input_sk = True
+        user2_input_sk = False
         user2_input_bl = False
         user2_input_st = False
         if len(stig) == 3:
@@ -40,19 +65,30 @@ while True:
             
             if user1_input_st and user2_input_sk:
                 stig.append("Blár")
+                
                 break
             elif user1_input_sk and user2_input_bl:
                 stig.append("Blár")
+                ljosoff()
                 break
             elif user1_input_bl and user2_input_st:
                 stig.append("Blár")
+                ljosoff()
                 break
             elif user2_input_st and user1_input_sk:
                 stig.append("Rautt")
+                ljosoff()
                 break
             elif user2_input_sk and user1_input_bl:
                 stig.append("Rautt")
+                ljosoff()
                 break
             elif user2_input_bl and user1_input_st:
+                stig.append("Rautt")
+                ljosoff()
+                break
+            elif user1_input_bl and user2_input_bl or user1_input_sk and user2_input_sk or user1_input_st and user2_input_st:
+                print("jafntefli")
+
                 stig.append("Rautt")
                 break
